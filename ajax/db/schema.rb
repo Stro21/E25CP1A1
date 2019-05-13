fdf14b1e9efe140fdc1bf9e46d793e1f13b8caeb
Eduardo Straub <eduardo.straub1@gmail.com>
2019-05-13 19:49:30 -0400

    Se agrego jquery

--- ajax/db/schema.rb
+++ ajax/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_13_232823) do
+ActiveRecord::Schema.define(version: 2019_05_13_233538) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
